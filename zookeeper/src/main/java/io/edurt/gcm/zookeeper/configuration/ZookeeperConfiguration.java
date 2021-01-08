63f8c7cef19c5c0b3460624461051bb0d3fad3c7
qianmoQ <shicheng@ttxit.com>
2021-01-08 18:03:38 +0800

    Remove zookeeper component configuration zookeeper prefix

--- zookeeper/src/main/java/io/edurt/gcm/zookeeper/configuration/ZookeeperConfiguration.java
+++ zookeeper/src/main/java/io/edurt/gcm/zookeeper/configuration/ZookeeperConfiguration.java
@@ -2,11 +2,11 @@ package io.edurt.gcm.zookeeper.configuration;
 
 public class ZookeeperConfiguration
 {
-    public static final String SERVICE = "component.zookeeper.service";
-    public static final String SESSION_TIMEOUT = "component.zookeeper.session-timeout";
-    public static final String CONNECTION_TIMEOUT = "component.zookeeper.connection-timeout";
-    public static final String RETRY = "component.zookeeper.retry";
-    public static final String NAMESPACE = "component.zookeeper.namespace";
+    public static final String SERVICE = "zookeeper.service";
+    public static final String SESSION_TIMEOUT = "zookeeper.session-timeout";
+    public static final String CONNECTION_TIMEOUT = "zookeeper.connection-timeout";
+    public static final String RETRY = "zookeeper.retry";
+    public static final String NAMESPACE = "zookeeper.namespace";
 
     private ZookeeperConfiguration() {}
 }
