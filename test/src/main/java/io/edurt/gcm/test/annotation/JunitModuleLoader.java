d7466d56bac688658a4022f1ccadc12124ea33d6
qianmoQ <shicheng@ttxit.com>
2021-01-26 23:19:09 +0800

    Support Test framework

---
+++ test/src/main/java/io/edurt/gcm/test/annotation/JunitModuleLoader.java
@@ -0,0 +1,15 @@
+package io.edurt.gcm.test.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+public @interface JunitModuleLoader
+{
+    Class<?>[] value();
+}
