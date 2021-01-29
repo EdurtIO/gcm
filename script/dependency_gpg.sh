270d5faf1c3143d17ab3ce0166743f30c2c633a4
qianmoQ <shicheng@ttxit.com>
2021-01-29 18:57:08 +0800

    Add checker script tool

--- script/dependency_gpg.sh
+++ script/dependency_gpg.sh
@@ -1,33 +1,20 @@
 #!/bin/bash
 
-:<<!
-GPG signature tool installation script
+: <<!
+    GPG signature tool installation script
 !
+
+source checker.sh
 
 dependency=gpg
 temp_dir=tmp
-temp_installation_dir=$temp_dir/gnupg-2.2.27
 download_remote=https://www.gnupg.org/ftp/gcrypt/gnupg/gnupg-2.2.27.tar.bz2
 download_file=$temp_dir/gnupg.tar.bz2
 
-mkdir $temp_dir
-
-function checker() {
-    which $dependency > /dev/null
-    if [ $? -eq 0 ]; then
-        return 0
-    else
-        return 1
-    fi
-}
+#mkdir $temp_dir
 
 function installation() {
     echo "Start installation $download_file from local cache"
-#    tar -xvzf $download_file -C $temp_dir
-#    cd $temp_installation_dir
-#    ./configure
-#    make
-#    `make install`
     brew install $dependency
 }
 
@@ -40,9 +27,10 @@ function download() {
     fi
 }
 
-checker
-if [ $? -eq 0 ]; then
-    echo 'Skip installation '$dependency
+checker_command $dependency
+if [ "$?" = "0" ]; then
+    echo "Command $dependency exists on system, skip installation $dependency"
 else
-    download
+    echo 1
+#    download
 fi
